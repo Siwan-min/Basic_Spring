f27b97d6f83956a52016015ed6c63454fdc9a1b6
minsw <minsw@asianaidt.com>
2022-08-19 07:47:43 +0900

    thymeleaf practice

--- springbasic/springbasic/src/main/java/com/example/springbasic/BasicController.java
+++ springbasic/springbasic/src/main/java/com/example/springbasic/BasicController.java
@@ -3,12 +3,21 @@ package com.example.springbasic;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 
 @Controller
 public class BasicController {
+
+    ArrayList<Menu> menus = new ArrayList<>() {{
+
+        add(new Menu("[게시판]", "/page1"));
+        add(new Menu("[자료실]", "/page2"));
+        add(new Menu("[Q&A]", "/page3"));
+
+    }};
 
     @GetMapping("/banner")
     public String banner(Model model) {
@@ -47,5 +56,60 @@ public class BasicController {
 
 
         return "memberlist";
+    }
+
+    @GetMapping("/page")
+    public String page(Model model) {
+
+        model.addAttribute("menus", menus);
+       // model.addAttribute("current", menus.get(2).getName());
+
+        return "page";
+    }
+
+    @GetMapping("/page1")
+    public String page1(Model model) {
+
+        model.addAttribute("menus", menus);
+        model.addAttribute("current", menus.get(0).getName());
+
+        return "page1";
+    }
+
+    @GetMapping("/page2")
+    public String page2(Model model) {
+
+        model.addAttribute("menus", menus);
+        model.addAttribute("current", menus.get(1).getName());
+
+        return "page2";
+    }
+
+    @GetMapping("/page3")
+    public String page3(Model model) {
+
+        model.addAttribute("menus", menus);
+
+        model.addAttribute("current", menus.get(2).getName());
+
+        return "page3";
+    }
+
+    @RequestMapping("/mission")
+    public String thymeleafapp(Model model){
+
+        model.addAttribute("genders", new boolean[]{true, true, false, true});
+
+        int[] seats = {1, 5, 10};
+
+        boolean[] list = new boolean[10];
+
+        for(int seat : seats)  {
+            list[seat-1] = true;
+        }
+
+        model.addAttribute("seats", list);
+
+        return "mission";
     }
 }
---
+++ springbasic/springbasic/src/main/java/com/example/springbasic/Menu.java
@@ -0,0 +1,15 @@
+package com.example.springbasic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+
+    private String name;
+    private String url;
+
+}
